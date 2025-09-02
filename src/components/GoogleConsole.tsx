9ef4b90f7ec700ea132958dfe3fdef3ff11dea5e
AGBESSI Gilchrist <gilchristagbessi92@gmail.com>
2025-09-02 16:19:21 +0100

    Reset local changes

---
+++ src/components/GoogleConsole.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const GoogleConsole: React.FC = () => {
+    return (
+        <meta name="google-site-verification" content="LUKqTDB1Xti6aVrI8QHtMAIIfpYAqkDYMWDCicuT2Wg" />
+    );
+};
