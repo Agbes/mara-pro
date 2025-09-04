ffcf7f93353d8cc9fca86cddbc928191b96dfa71
AGBESSI Gilchrist <gilchristagbessi92@gmail.com>
2025-09-04 17:38:19 +0100

    Autre modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,21 @@ import Testimonials from "@/components/Acceuille/Testimonials/Testimonials";
 
 import { generateJSONLD, generateStaticMetadata } from "@/lib/seo";
 import { seoAccueil } from "@/data/seoData";
+import Navbar from "@/components/Navigation/Navbar";
+import Footer from "@/components/Navigation/Footer/Footer";
 
 export const metadata = generateStaticMetadata(seoAccueil);
 
 export default function Home() {
   return (
     <>
+          <Navbar /> 
       <HeroSection />
       <DomainsSection />
       <Testimonials />
       <CallToAction />
       <ArticlesSection />
+      <Footer />
 
       <script
         type="application/ld+json"
