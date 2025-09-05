e5903bf6b12b35ee655e9a159d3e06572c05178a
AGBESSI Gilchrist <gilchristagbessi92@gmail.com>
2025-09-05 11:24:18 +0100

    Autre modification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,31 +2,34 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import SessionWrappers from "@/lib/sessionWrappers";
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 export const metadata: Metadata = {
-  title: "Medium Moussa Ali",
-  description: "Puissant medium marabout Moussa Ali",
-  icons: {
-    icon: "/Ali-moussa.png",
-  },
-  other: {
-    "google-site-verification": process.env.NEXT_PUBLIC_GOOGLE_CONSOLE || "",
-  },
+    title: "Medium Moussa Ali",
+    description: "Puissant medium marabout Moussa Ali",
+    icons: {
+        icon: "/Ali-moussa.png",
+    },
+    other: {
+        "google-site-verification": process.env.NEXT_PUBLIC_GOOGLE_CONSOLE || "",
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <SessionWrappers>
-      <html lang="fr">
+    return (
+        <SessionWrappers>
+            <html lang="fr">
+                <GoogleTagManager gtmId="GTM-KSDX9JB8" />
 
-        <body className="antialiased text-slate-800">
-          {children}
-        </body>
-      </html>
-    </SessionWrappers>
-  );
+                <body className="antialiased text-slate-800">
+                    {children}
+                </body>
+            </html>
+        </SessionWrappers>
+    );
 }
