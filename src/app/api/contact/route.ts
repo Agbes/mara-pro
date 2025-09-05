4b520f360d3212aedcf8c3248bfa4aeba4d9da74
AGBESSI Gilchrist <gilchristagbessi92@gmail.com>
2025-09-05 09:18:16 +0100

    Autre modification

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -15,8 +15,8 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
